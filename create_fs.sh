f8e021cf0d3f6237cdf1bad4a5faeb09715111a8
Rim Rakhimov <rimrakhimov@gmail.com>
2020-03-22 19:27:14 +0300

    filesystem creation

--- create_fs.sh
+++ create_fs.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 IMG_NAME="fs_image.img"
 
@@ -7,7 +7,7 @@ if ! test -f $IMG_NAME; then
 	printf "\nImage file is creating\n"
 	dd if=/dev/zero of=$IMG_NAME bs=100M count=10
 else
-	printf "\nImage file has already existed\n"
+	printf "\nImage file already exists\n"
 fi
 
 # create a 'ext4' filesystem if it does not already exist
